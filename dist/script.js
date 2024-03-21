d0dfb559bb1976bc3edd8aae1fc189805888284d
BORIS <boris200723@gmail.com>
2024-03-22 00:10:31 +0300

    Done adaptive to mobile-1

---
+++ dist/script.js
@@ -0,0 +1,7 @@
+const mainInfo = document.querySelector("ul").childNodes[5],
+  firstSection = document.querySelector(".first-section");
+
+mainInfo.addEventListener("click", (e) => {
+  firstSection.scrollIntoView({ behavior: "smooth" });
+});
+
