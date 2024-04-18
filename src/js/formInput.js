d7afd30e9edb6cdbc8b884762ef9b715a5a06c36
BORIS <boris200723@gmail.com>
2024-04-18 22:51:11 +0300

    Commit 1.0.0

--- src/js/formInput.js
+++ src/js/formInput.js
@@ -2,24 +2,13 @@ import { pull } from "./mySQL.js";
 const name = document.querySelector("[name=name]"),
   email = document.querySelector("[name=email]"),
   phone = document.querySelector("[name=phone]"),
-  rule = document.querySelector("[name=rule]")
-// находим форму в документе
-const submit = document.querySelector(".submit__go");
-// указываем адрес отправки формы (нужно только в начале примера)
-form.action = URL_APP;
-// вспомогательная функция проверки заполненности формы
-function isFilled(details) {
-  const { name, email, phone, rule } = details;
-  if (!name) return false;
-  else if (!email) return false;
-  else if (!phone) return false;
-  else if (!rule) return false;
-  return true;
-}
-// навешиваем обработчик на отправку формы
+  rule = document.querySelector("[name=rule]"),
+ submit = document.querySelector(".submit__go");
+
 submit.addEventListener("click", async (e) => {
-  // отменяем действие по умолчаниюВ
+  // Отменяем действие по умолчаниюВ
   e.preventDefault();
+
   // Класс нового пользователя
   class User {
     constructor(name, email, phone, rule) {
@@ -32,18 +21,17 @@ submit.addEventListener("click", async (e) => {
 
   // собираем данные из элементов формы
   const user = new User(
-    name.value.trim(),
-    email.value.trim(),
-    phone.value.trim(),
+    name.value,
+    email.value,
+    phone.value,
     rule.checked
   );
+  
   // если поля не заполнены - прекращаем обработку
-  if (!isFilled(user)) return;
-  // подготавливаем данные для отправки
-  let formBody = [];
-
-  // склеиваем параметры в одну строку
-  formBody = formBody.join("&");
+  if (!name.value) return 
+  else if (!email.value) return 
+  else if (!phone.value) return 
+  else if (!rule.checked) return 
 
   // Отправка данных в mySQL
   pull(user.name, user.email, user.phone, user.rule)
