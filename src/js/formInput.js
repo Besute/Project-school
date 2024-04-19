1aa2ef5b552e2ec63ae764aec6f45f2d31c220b2
BORIS <boris200723@gmail.com>
2024-04-19 16:26:00 +0300

    Some optimisation

--- src/js/formInput.js
+++ src/js/formInput.js
@@ -8,19 +8,6 @@ const name = document.querySelector("[name=name]"),
 submit.addEventListener("click", async (e) => {
   // Отменяем действие по умолчаниюВ
   e.preventDefault();
-  // Класс нового пользователя
-  class User {
-    constructor(name, email, phone, rule) {
-      this.name = name;
-      this.email = email;
-      this.phone = phone;
-      this.rule = rule;
-    }
-  }
-
-  // собираем данные из элементов формы
-  const user = new User(name.value, email.value, phone.value, rule.checked);
-
   // если поля не заполнены - прекращаем обработку
   if (!name.value) return;
   else if (!email.value) return;
